cb99d610cb18c90a3cfa1bcab60e688845113a64
onurabdulaji <onurabdulaji@gmail.com>
2025-03-16 00:04:22 +0100

    UnitOfWork ve GenericRepository Design eklendi

---
+++ Core/OAPortfolio.Application/Interfaces/IRepositories/IReadRepository.cs
@@ -0,0 +1,7 @@
+﻿using OAPortfolio.Domain.Commons.Abstracts;
+
+namespace OAPortfolio.Application.Interfaces.IRepositories;
+
+public interface IReadRepository<T> where T : class, IEntityBase, new()
+{
+}
---
+++ Infastructure/OAPortfolio.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OAPortfolio.Application.Interfaces.IRepositories;
+using OAPortfolio.Domain.Commons.Abstracts;
+using OAPortfolio.Persistence.Context;
+
+namespace OAPortfolio.Persistence.Repositories;
+
+public class ReadRepository<T> : IReadRepository<T> where T : class, IEntityBase, new()
+{
+    private readonly AppDbContext _context;
+
+    public ReadRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    private DbSet<T> Table { get => _context.Set<T>(); }
+}
