0f3ab097b2a81c2c17b76a5e4fda6ec12f4ddb5b
onurabdulaji <onurabdulaji@gmail.com>
2025-03-15 23:45:14 +0100

    Persistence eklentileri eklendi. Migration yapildi.

---
+++ Infastructure/OAPortfolio.Persistence/Extensions/PersistenceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OAPortfolio.Domain.Entities;
+using OAPortfolio.Persistence.Context;
+
+namespace OAPortfolio.Persistence.Extensions;
+
+public static class PersistenceExtensions
+{
+    public static void AddPersistenceLayer(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentity<User, Role>(opt =>
+        {
+            opt.Password.RequireNonAlphanumeric = false;
+            opt.Password.RequiredLength = 2;
+            opt.Password.RequireLowercase = false;
+            opt.Password.RequireUppercase = false;
+            opt.Password.RequireDigit = false;
+            opt.SignIn.RequireConfirmedEmail = false;
+        })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+    }
+}
