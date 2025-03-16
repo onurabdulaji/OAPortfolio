d528ca393180eb1697550c879969a3442169ca96
onurabdulaji <onurabdulaji@gmail.com>
2025-03-16 01:14:12 +0100

    Identity Seed eklendi

--- Infastructure/OAPortfolio.Persistence/Extensions/PersistenceExtensions.cs
+++ Infastructure/OAPortfolio.Persistence/Extensions/PersistenceExtensions.cs
@@ -7,13 +7,14 @@ using OAPortfolio.Application.Interfaces.IUnitOfWorks;
 using OAPortfolio.Domain.Entities;
 using OAPortfolio.Persistence.Context;
 using OAPortfolio.Persistence.Repositories;
+using OAPortfolio.Persistence.Seed;
 using OAPortfolio.Persistence.UnitOfWorks;
 
 namespace OAPortfolio.Persistence.Extensions;
 
 public static class PersistenceExtensions
 {
-    public static void AddPersistenceLayer(this IServiceCollection services,IConfiguration configuration)
+    public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
         {
@@ -27,7 +28,7 @@ public static class PersistenceExtensions
             opt.Password.RequireLowercase = false;
             opt.Password.RequireUppercase = false;
             opt.Password.RequireDigit = false;
-            opt.SignIn.RequireConfirmedEmail = false;
+            opt.SignIn.RequireConfirmedEmail = true;
         })
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
@@ -37,5 +38,16 @@ public static class PersistenceExtensions
         services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+    public static async Task UseIdentityDatabaseSeederAsync(this IServiceProvider services)
+    {
+        using (var scope = services.CreateScope())
+        {
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+
+            await IdentitySeeder.SeedAsync(userManager, roleManager);
+        }
     }
 }
